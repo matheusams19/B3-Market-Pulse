c7b04e48c12f8db4e7e1a6761f9f867b8211b379
Matheus Alexandre <matheusams19@gmail.com>
2026-02-18 18:57:56 -0300

    Update Backtest.py

--- app/pages/Backtest.py
+++ app/pages/Backtest.py
@@ -2,6 +2,8 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 from sqlalchemy import create_engine
+from interface import criar_menu
+criar_menu()
 
 st.set_page_config(page_title="Backtest", layout="wide")
 st.title("📈 Backtest — Estratégia MA20 > MA50")
